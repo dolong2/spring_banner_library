239db8407581d3229552da35c4c203f065588007
jeayoung <k01066624566@gmail.com>
2023-03-26 23:15:24 +0900

    gradle 설정 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("io.spring.dependency-management") version "1.1.0"
     kotlin("jvm") version "1.7.22"
     kotlin("plugin.spring") version "1.7.22"
+    application
 }
 
 group = "library"
@@ -30,4 +31,8 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+application{
+    mainClass.set("library.banner.BannerGenerator")
 }
